4a1ab104cbd168d49cf95535407446cc88b40d8d
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-10-01 16:45:30 +0300

    assignment 3 completed

--- Assets/Scripts/MathUtils.cs
+++ Assets/Scripts/MathUtils.cs
@@ -7,4 +7,25 @@ public class MathUtils
         var scalarProjection = Vector3.Dot(rayDirection, surfaceNormal);
         return rayDirection - 2 * scalarProjection * surfaceNormal;
     }
+
+    public static Vector3 LocalToWorld(Vector3 localPoint, Transform transform)
+    {
+        var right = (Vector3)transform.worldToLocalMatrix.GetColumn(0).normalized;
+        var up = (Vector3)transform.worldToLocalMatrix.GetColumn(1).normalized;
+        var forward = (Vector3)transform.worldToLocalMatrix.GetColumn(2).normalized;
+        var position = transform.position;
+
+        var dotRight = Vector3.Dot(localPoint, right);
+        var dotUp = Vector3.Dot(localPoint, up);
+        var dotForward = Vector3.Dot(localPoint, forward);
+        
+        position += new Vector3(dotRight, dotUp, dotForward);
+
+        return position;
+    }
+
+    public static Vector3 WorldToLocal(Vector3 worldPoint, Vector3 referencePoint)
+    {
+        return worldPoint - referencePoint;
+    }
 }
---
+++ Assets/Scripts/Transformation.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Transformation : MonoBehaviour
+{
+    [SerializeField] private Vector3 localPosition;
+
+    private void OnDrawGizmos() {
+
+        var worldPosition = MathUtils.LocalToWorld(localPosition, transform);
+        Gizmos.DrawSphere(worldPosition, 0.1f);
+
+        var worldToLocalPos = MathUtils.WorldToLocal(worldPosition, transform.position);
+
+        var labelOffset = new Vector3(0, 0.2f, 0f);
+        Handles.Label(worldPosition + labelOffset, new GUIContent("local position: " + worldToLocalPos + "\nworld position: " + worldPosition));
+    }
+
+}
